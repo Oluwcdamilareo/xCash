2e72b8209bb712840e79253771c1388019892328
Oluwadamilare Olusakin <oluwadamilareolusakin@pop-os.localdomain>
2019-01-02 10:50:29 +0100

    Production build

---
+++ build/precache-manifest.b7eebe2305b41a387942d3776e79635c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61d4c0edfc4c2c59b794",
+    "url": "/static/css/main.032bdf30.chunk.css"
+  },
+  {
+    "revision": "61d4c0edfc4c2c59b794",
+    "url": "/static/js/main.61d4c0ed.chunk.js"
+  },
+  {
+    "revision": "cdd0f5d186b05eb4a119",
+    "url": "/static/js/1.cdd0f5d1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e0bc458591c2207416bcb6a8fd3f0969",
+    "url": "/index.html"
+  }
+];
