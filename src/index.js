9c95e5ecfe5cf36df1e71bea77ea3fb5ae03f3be
Oluwadamilare Olusakin <oluwadamilareolusakin@pop-os.localdomain>
2019-01-04 00:11:53 +0100

    Fix service worker and home buttons

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker.js';
+import register from './registerServiceWorker.js';
 
 const root = document.getElementById('root')
 
 ReactDOM.render(<App/>,root)
 
-registerServiceWorker();
+register();
